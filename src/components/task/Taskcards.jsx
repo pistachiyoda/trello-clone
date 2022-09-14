3ce0efbf60afd0e66758962aff08508d2772df60
mai <fujisawamai21@gmail.com>
2022-09-14 23:10:08 +0900

    カラムを追加できるようにした。

--- src/components/task/Taskcards.jsx
+++ src/components/task/Taskcards.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TaskCard from './TaskCard';
 import AddTaskCardButton from './button/AddTaskCardButton';
 import styled from 'styled-components';
 
+const TaskCardArea = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  flex-wrap: wrap;
+`;
+
 export const TaskCards = () => {
+  const [taskCardsList, settaskCardsList] = useState([
+    {
+      id: 0,
+      draggableid: 0,
+    },
+  ]);
   return (
-    <div>
-      <TaskCard />
-      <AddTaskCardButton />
-    </div>
+    <TaskCardArea>
+      {taskCardsList.map((taskCardList) => {
+        return <TaskCard key={taskCardList.id} />;
+      })}
+      <AddTaskCardButton taskCardsList={taskCardsList} settaskCardsList={settaskCardsList} />
+    </TaskCardArea>
   );
 };
--- src/components/task/button/AddTaskCardButton.jsx
+++ src/components/task/button/AddTaskCardButton.jsx
@@ -1,7 +1,44 @@
 import React from 'react';
+import styled from 'styled-components';
+import { v4 as uuidv4 } from 'uuid';
 
-const AddTaskCardButton = () => {
-  return <div></div>;
+const Wrapper = styled.div`
+  margin-left: 1%;
+  margin-top: 25px;
+`;
+const AddButton = styled.button`
+  cursor: pointer;
+  margin-right: 9px;
+  border: none;
+  padding: 10px 18px;
+  border-radius: 9999px;
+  font-size: 30px;
+  background-color: rgb(221, 194, 162);
+  box-shadow: 3px 3px 8px 1px black;
+  transition: all 0.3s;
+  &:hover {
+    box-shadow: none;
+    transform: translate(3px, 3px);
+  }
+`;
+
+const AddTaskCardButton = (props) => {
+  const { taskCardsList, settaskCardsList } = props;
+
+  const addTaskCard = () => {
+    settaskCardsList([
+      ...taskCardsList,
+      {
+        id: uuidv4(),
+        draggableid: `task-${uuidv4()}`,
+      },
+    ]);
+  };
+  return (
+    <Wrapper>
+      <AddButton onClick={addTaskCard}>+</AddButton>
+    </Wrapper>
+  );
 };
 
 export default AddTaskCardButton;
